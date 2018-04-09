4bbeb07d6bffe6de5addf9e66ee10a53679a783f
MinSu Kim <net94@naver.com>
2018-04-09 11:46:56 +0900

    임시변수(totalAmount)를 메서드(getTotalCharge())호출로 전환

--- src/main/java/kr/or/dgit/refactoring_advance/Customer.java
+++ src/main/java/kr/or/dgit/refactoring_advance/Customer.java
@@ -32,15 +32,21 @@ public class Customer {
 //          이번에 대여하는 비디오 정보와 대여료를 출력
             result.append("\t"+each.getMovie().getTitle());
             result.append("\t"+String.valueOf(each.getCharge())+"\n");
-            
-//          현재까지 누적된 총 대여료
-            totalAmount += each.getCharge();
+
         }//end of for-loop
         
 //      푸터행 추가
-        result.append("누적 대여료 : " + String.valueOf(totalAmount) + "\n");
+        result.append("누적 대여료 : " + String.valueOf(getTotalCharge()) + "\n");
         result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
         
         return result.toString();
     }//end of method statement
+
+	private double getTotalCharge() {
+		double result = 0;
+		for(Rental rental : rentals) {
+			result += rental.getCharge();
+		}
+		return result;
+	}
 }
